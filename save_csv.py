1bc1b059cb99381c0f5c20c77db78c5043e312e8
Geunmo Kim <kimgeunmo@gmail.com>
2021-04-21 21:29:06 +0200

    added comments to save_csv.py

--- save_csv.py
+++ save_csv.py
@@ -2,9 +2,16 @@ import csv
 
 
 def save_to_file(search, jobs):
+    # creating empty csv file with {search} as name and in write-only mode
     file = open(f"{search.lower()}.csv", mode="w")
+
+    # writer function let's you to write in the argument file
     writer = csv.writer(file)
+
+    # adding first row with writerow
     writer.writerow(["title", "company", "location", "link"])
+
+    # adding each row to the csv file
     for job in jobs:
         writer.writerow(list(job.values()))
     return
